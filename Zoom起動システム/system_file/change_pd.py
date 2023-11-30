acd7cc05e4c2e2c62b71244ae03aa4962820c917
1 <[regu7828@gmail.com]>
2023-11-30 13:39:32 +0900

    pathの変更

--- Zoom起動システム/system_file/change_pd.py
+++ Zoom起動システム/system_file/change_pd.py
@@ -2,7 +2,7 @@ import pandas as pd
 import json
 # CSVファイルの読み込み(pandasを返す)
 def change_pd():
-    with open('test.json', 'r', encoding='utf-8') as file:
+    with open('Zoom起動システム/test.json', 'r', encoding='utf-8') as file:
         reader = json.load(file)
     pd_data = pd.DataFrame(reader)
     json_data = pd_data.to_json(orient='records')  # 'records'は行ごとに辞書として格納
